d8b556102c0a3c9957e4d1ec88feadf4ae409a9a
Lazarus <lazaruss2024@gmail.com>
2025-03-17 15:11:20 +0700

    Completed Ado.Net Read

--- KLTDotNetCore.ConsoleApp/Program.cs
+++ KLTDotNetCore.ConsoleApp/Program.cs
@@ -1,3 +1,47 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Microsoft.Data.SqlClient;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
 
+Console.WriteLine("Hello, World!");
+
+// Sql Connection
+// Ctrl + .
+
+SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
+stringBuilder.DataSource = "localhost";
+stringBuilder.InitialCatalog = "DotNetTrainingBatch4";
+stringBuilder.UserID = "sa";
+stringBuilder.Password = "sa123";
+stringBuilder.TrustServerCertificate = true;
+
+SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
+// Optional Connection Way
+
+connection.Open();
+Console.WriteLine("Connection Opened.");
+
+string query = "select * from tbl_blog";
+SqlCommand command = new SqlCommand(query, connection);
+SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(command);
+DataTable dt = new DataTable();
+sqlDataAdapter.Fill(dt);
+
+connection.Close();
+Console.WriteLine("Connection Closed.");
+
+
+// dataset => datatable
+// datatable => datarow
+// datarow => datacolumn
+
+foreach(DataRow dr in dt.Rows)
+{
+	Console.WriteLine(" Blog Id => " + dr["BlogId"]);
+	Console.WriteLine(" Blog Title => " + dr["BlogTitle"]);
+	Console.WriteLine(" Blog Author => " + dr["BlogAuthor"]);
+	Console.WriteLine(" Blog Content => " + dr["BlogContent"]);
+	Console.WriteLine("-------------------");
+}
+
+Console.ReadKey();
 
---
+++ README.md
@@ -0,0 +1,8 @@
+UI + Business Logic + Data access + Db
+
+Mobile, Web => API => Database
+
+
+C# => Db
+
+KlTDotNetCore
